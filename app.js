1716ee279ecd15532782fb13b89a6d5f87a98a3e
AhsanTipu <syedahsan2305@gmail.com>
2020-07-15 14:38:04 -0700

    JS CHapters 1-67

--- app.js
+++ app.js
@@ -204,3 +204,45 @@
 // count--;
 //     a.innerHTML=count;
 // }
+
+// CHAPTER 49-52
+// Q1
+// var a1;
+// function display(){
+//     var a=document.getElementById("f1");
+//     var c=a.value;
+//     var b=document.getElementById("f2");
+//     a1=b.value;
+//     document.write("Username : "+c+"<br>"+"Password : "+a1);
+// }
+
+// Q2
+// var text;
+// var p;
+// function readMore(){
+//     text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, aspernatur at! Repellat, temporibus eaque. Magni dicta nesciunt corrupti distinctio molestiae unde? Consectetur quod voluptate, rem expedita veritatis neque ducimus nostrum?";
+//      p=document.getElementById("pera");
+// p.innerText=text;
+// }
+
+// Q3
+// var a;
+// var b;
+// var z=0;
+// var y=1;
+// var l1=[];
+// var l2=[];
+// function fetch(){
+    
+
+//     var table=document.getElementById("mytable");
+//         var row=table.insertRow(y);
+//         l1[z]=document.getElementById("f1").value;
+//         l2[z]=document.getElementById("f2").value;
+//         var cell1=row.insertCell(0);
+//         var cell2=row.insertCell(1);
+//         cell1.innerHTML = l1[z];
+//         cell2.innerHTML =l2[z];
+//         y++;
+//         z++;
+// }
